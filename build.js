2993094648bce810d7a4061dae487e2cbe32941c
Aldo Huapaya <91620229+aldhua@users.noreply.github.com>
2024-07-24 00:10:28 -0500

    Update build.js

--- build.js
+++ build.js
@@ -1,29 +1,8 @@
-import { register } from '@tokens-studio/sd-transforms';
 import StyleDictionary from 'style-dictionary';
+import { register } from '@tokens-studio/sd-transforms';
 
-// will register them on StyleDictionary object
-// that is installed as a dependency of this package.
-register(StyleDictionary);
-
-const sd = new StyleDictionary({
-  // make sure to have source match your token files!
-  // be careful about accidentally matching your package.json or similar files that are not tokens
-  source: ['tokens/**/*.json'],
-  preprocessors: ['tokens-studio'], // <-- since 0.16.0 this must be explicit
-  platforms: {
-    css: {
-      transformGroup: 'tokens-studio', // <-- apply the tokens-studio transformGroup to apply all transforms
-      transforms: ['name/kebab'], // <-- add a token name transform for generating token names, default is camel
-      buildPath: 'build/css/',
-      files: [
-        {
-          destination: 'variables.css',
-          format: 'css/variables',
-        },
-      ],
-    },
-  },
-});
-
-await sd.cleanAllPlatforms();
-await sd.buildAllPlatforms();
+// sd-transforms, 2nd parameter for options can be added
+// See docs: https://github.com/tokens-studio/sd-transforms
+register(StyleDictionary,{
+  excludeParentKeys:true,
+  });
